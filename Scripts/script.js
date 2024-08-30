3bd29ecc61fd95b291bbb38dce5576997cb568ae
Jean <115487924+JeanYone@users.noreply.github.com>
2024-08-30 01:25:59 -0300

    Carrosel 5

--- Scripts/script.js
+++ Scripts/script.js
@@ -44,36 +44,17 @@ const form = document.querySelector("#form");
             return Password.length >= minDigits;
         }
 
-let slideIndex = 1;
-showSlides(slideIndex);
+        const slidesContainer = document.getElementById("slides-container");
+        const slide = document.querySelector(".slide");
+        const prevButton = document.getElementById("slide-arrow-prev");
+        const nextButton = document.getElementById("slide-arrow-next");
 
-// Função para avançar/retroceder slides
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+        nextButton.addEventListener("click", () => {
+        const slideWidth = slide.clientWidth;
+        slidesContainer.scrollLeft += slideWidth;
+        });
 
-// Função para selecionar um slide específico
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-// Função para mostrar o slide atual
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
+        prevButton.addEventListener("click", () => {
+        const slideWidth = slide.clientWidth;
+        slidesContainer.scrollLeft -= slideWidth;
+        });
