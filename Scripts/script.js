efe2159caf0ba8e02e33ad2d0f5e0422717a5c0b
Jean <115487924+JeanYone@users.noreply.github.com>
2024-08-30 01:09:46 -0300

    Carrosel 1

--- Scripts/script.js
+++ Scripts/script.js
@@ -43,3 +43,37 @@ const form = document.querySelector("#form");
         function validatePassword(Password, minDigits) {
             return Password.length >= minDigits;
         }
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Função para avançar/retroceder slides
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Função para selecionar um slide específico
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+// Função para mostrar o slide atual
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
